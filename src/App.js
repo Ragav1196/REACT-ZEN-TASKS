1b8855a6ee4704a3e3a03b1a56be469b46aeb15c
Ragav1196 <ragavinrap@gmail.com>
2022-01-11 04:18:58 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
 import "./App.css";
-import { TopBar } from "./SESSIONS/SESSION 28/TopBar";
-import { MainContent } from "./SESSIONS/SESSION 28/MainContent";
-import { SideMenuBar } from "./SESSIONS/SESSION 28/SideMenuBar";
-import { AdminPanel } from "./SESSIONS/SESSION 28/PRODUCTS LIST/AdminPanel";
-import { AdminPanel as UsersAdminPanel } from "./SESSIONS/SESSION 28/USERS LIST/AdminPanel";
-import { Switch, Route } from "react-router-dom";
 
 export default function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <section className="wholeContent">
-            <SideMenuBar />
-            <div className="middleWholeContent">
-              <TopBar />
-              <MainContent />
-            </div>
-          </section>
-        </Route>
-
-        <Route path="/products-list">
-          <div className="adminPanel">
-            <SideMenuBar />
-            <AdminPanel />
-          </div>
-        </Route>
-
-        <Route path="/users-list">
-          <div className="adminPanel">
-            <SideMenuBar />
-            <UsersAdminPanel />
-          </div>
-        </Route>
-      </Switch>
-    </div>
-  );
+  return <div>RAGAV</div>;
 }
