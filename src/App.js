929844af6b85acf56ba9f4dc07818e398c54a439
Ragav1196 <ragavinrap@gmail.com>
2022-01-11 09:41:34 +0530

    Layout completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,76 @@
 import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import { UsersList } from "./UsersList";
 
 export default function App() {
-  return <div>RAGAV</div>;
+  // TO TOGGLE HIDE ADD BUTTON
+  const [addBtn, setAddBtn] = useState(true);
+
+  // TO TOGGLE HIDE ADD FORM
+  const [addForm, setAddForm] = useState(false);
+
+  return (
+    <section className="addUser">
+      <article>
+        <h1>USERS LIST</h1>
+        {addBtn ? (
+          <>
+            <Button
+              onClick={() => {
+                setAddForm(!addForm);
+                setAddBtn(false);
+              }}
+              id="addButton"
+              variant="contained"
+              size="small"
+              color="success"
+            >
+              ADD USER
+            </Button>
+          </>
+        ) : (
+          ""
+        )}
+        {addForm ? (
+          <>
+            <div>
+              <TextField
+                id="outlined-basic"
+                size="small"
+                label="Name"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-basic"
+                size="small"
+                label="Picture URL"
+                variant="outlined"
+              />
+            </div>
+            <Button
+              onClick={() => {
+                setAddBtn(true);
+                setAddForm(false);
+              }}
+              id="addButton"
+              variant="contained"
+              size="small"
+              color="success"
+            >
+              ADD
+            </Button>
+          </>
+        ) : (
+          ""
+        )}
+      </article>
+      <article>
+        <UsersList />
+      </article>
+    </section>
+  );
 }
+
+
